706b06631b08632745d45eb448afd2dd2d0ff777
Charles Tolentino <chatol01@roberthalf.com>
2020-06-03 12:08:36 -0700

    working modal with left slide effect

--- cards.js
+++ cards.js
@@ -5,9 +5,51 @@ import projects from './projects.js';
 const cardDeck = document.getElementById('cardDeck');
 
 // Create new Card
-const newCard = ({name, description, tech}) => `<div class="card modal-btn"><div class="container"><h2>${name}</h2><p>${description}</p><p class="card-tech"><b>Utilized</b>: ${tech}</p></div></div>`;
+const cardTemplate = ( id, {name, cardDesc, tech} ) => {
+  return `
+  <div class="card modal-btn" id='${id}'>
+    <div class="container">
+      <h2>${name}</h2>
+      <p>${cardDesc}</p>
+      <p>
+        <b>Utilized</b>: ${tech}
+      </p>
+    </div>
+  </div>`;
+}
 
 // Generate Project Cards
-for(let project of projects) {
-  cardDeck.innerHTML += newCard(project);
+for(let project in projects) {
+  cardDeck.innerHTML += cardTemplate(project, projects[project]);
 }
+
+const cards = document.getElementsByClassName('card');
+const modal = document.getElementsByClassName('modal')[0];
+const modalOverlay = document.getElementById('overlay');
+const modalCloseButton = document.getElementById('close');
+const modalHeader = document.getElementById('modalHeader');
+
+const setModalContent = header => {
+  modalHeader.textContent = header;
+}
+
+function openModal() {
+  if(!modal.classList.contains('show')) {
+    modal.classList.add('show');
+    setModalContent(this.id)
+  }
+}
+
+function closeModal() {
+  if(modal.classList.contains('show'))
+    modal.classList.remove('show');
+}
+
+for(let card of cards) {
+  let currModal = modal.cloneNode(true);
+  currModal.innerHTML += 'YES'
+  card.addEventListener('click', openModal);
+}
+
+modalOverlay.addEventListener('click', closeModal);
+modalCloseButton.addEventListener('click', closeModal);
--- projects.js
+++ projects.js
@@ -1,33 +1,33 @@
-export default [
-    {
-      name: 'SeekIt',
-      description: 'A web and mobile platform that allows instructors to take education beyond the classroom.',
-      tech: 'React, React Native, Firebase, Bootstrap'
-    },
-    {
-      name: 'Journaling',
-      description: 'A simple web app for writing and viewing journal entries in chronological order.',
-      tech: 'React, Redux, Redux Saga, Bootstrap, Node, Express, MongoDB'
-    },
-    {
-      name: 'LilBill',
-      description: 'An iOS app that uses the camera to identify the value of a dollar bill.',
-      tech: 'React Native, TuriCreate'
-    },
-    {
-        name: 'Falcon Tweeter',
-        description: 'A twitter bot that tweets a Captain Falcon move every 6 hours.',
-        tech: 'Node, Twitter API, Heroku'
-    },
-    {
-        name: 'HackMerced 4.0',
-        description: "The Pre-Registration, Main, and Live site for HackMerced's 4th annual hackathon",
-        tech: 'React, Node, Express, MongoDB'
-    },
-    {
-        name: 'Myo HMI',
-        description: 'An Android app for low-cost gesture recognition research conducted by ICE Lab in SFSU.',
-        tech: 'Android Studio, Myo-Armband'
-    }
-  ];
+export default {
+  seekItCard: {
+    name: 'SeekIt',
+    cardDesc: 'A web and mobile platform that allows instructors to take education beyond the classroom.',
+    tech: 'React, React Native, Firebase, Bootstrap'
+  },
+  journalingCard: {
+    name: 'Journaling',
+    cardDesc: 'A simple web app for writing and viewing journal entries in chronological order.',
+    tech: 'React, Redux, Redux Saga, Bootstrap, Node, Express, MongoDB'
+  },
+  lilBillCard: {
+    name: 'LilBill',
+    cardDesc: 'An iOS app that uses the camera to identify the value of a dollar bill.',
+    tech: 'React Native, TuriCreate'
+  },
+  falconCard: {
+    name: 'Falcon Tweeter',
+    cardDesc: 'A twitter bot that tweets a Captain Falcon move every 6 hours.',
+    tech: 'Node, Twitter API, Heroku'
+  },
+  hackmercedCard: {
+    name: 'HackMerced 4.0',
+    cardDesc: "The Pre-Registration, Main, and Live site for HackMerced's 4th annual hackathon",
+    tech: 'React, Node, Express, MongoDB'
+  },
+  myoCard: {
+    name: 'Myo HMI',
+    cardDesc: 'An Android app for low-cost gesture recognition research conducted by ICE Lab in SFSU.',
+    tech: 'Android Studio, Myo-Armband'
+  }
+};
 
